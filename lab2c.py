8b014efbb7ee93fa59b2c7b2460ab3dc736ce7f0
Ebrahim Patel <epatel16@myseneca.ca>
2025-01-26 19:30:27 -0500

    Completed LAb

--- lab2c.py
+++ lab2c.py
@@ -3,9 +3,8 @@
 import sys
 
 
-name = sys.argv[1]  
-age = int(sys.argv[2])  
+name = sys.argv[1]
+age = int(sys.argv[2])
 
 
 print("Hi " + name + ", you are " + str(age) + " years old.")
-
--- lab2d.py
+++ lab2d.py
@@ -2,14 +2,15 @@
 
 import sys
 
+# Check if exactly two additional arguments are provided
 if len(sys.argv) != 3:
     print(f"Usage: {sys.argv[0]} name age")
-    sys.exit(1)  
+    sys.exit(0)  # Exit with code 0 to indicate a successful termination
 
+# Assign arguments to variables
+name = sys.argv[1]
+age = int(sys.argv[2])
 
-name = sys.argv[1]  
-age = int(sys.argv[2])  
-
-
+# Output the result
 print("Hi " + name + ", you are " + str(age) + " years old.")
 
